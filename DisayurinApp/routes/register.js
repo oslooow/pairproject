b5b5174661f0da4b1ca528a528fcc91f0b527fd9
oslooow <122131992+oslooow@users.noreply.github.com>
2023-03-10 08:41:37 +0700

    lastcommit

--- DisayurinApp/routes/register.js
+++ DisayurinApp/routes/register.js
@@ -5,4 +5,8 @@ router.get('/register', Controller.register)
 
 router.post('/register', Controller.saveRegister)
 
+router.get('/register/admin', Controller.registerAdmin)
+
+router.post('/register/admin', Controller.saveRegister)
+
 module.exports = router
